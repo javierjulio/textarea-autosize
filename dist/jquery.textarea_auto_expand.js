fab05eb9bdc72eb742ddfa5d674f893c10fad1b7
Javier Julio <jjfutbol@gmail.com>
2014-03-18 22:53:13 -0400

    Update comments.

--- dist/jquery.textarea_auto_expand.js
+++ dist/jquery.textarea_auto_expand.js
@@ -4,21 +4,22 @@
       var textarea = $(this);
       var height = textarea.outerHeight();
       var diff = parseInt(textarea.css('paddingBottom')) + parseInt(textarea.css('paddingTop'));
-      
+
       function containsText(value) {
         return (value.replace(/\s/g, '').length > 0);
       }
-      
-      // special case for Firefox where scrollHeight isn't full height on border-box
+
+      // Firefox: scrollHeight isn't full height on border-box
       if (this.scrollHeight + diff <= height) {
         diff = 0;
       }
-      
+
       if (containsText(this.value)) {
         textarea.height(this.scrollHeight);
       }
-      
-      textarea.on('input keyup', function(event) { // keyup is required for IE to properly reset height when deleting text
+
+      // keyup is required for IE to properly reset height when deleting text
+      textarea.on('input keyup', function(event) {
         $(this)
           .height('auto')
           .height(this.scrollHeight - diff);
