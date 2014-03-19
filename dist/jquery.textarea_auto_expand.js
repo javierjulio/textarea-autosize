2e43e8d2785bfda681ed4e352e5ad4d4ea4a9ad2
Javier Julio <jjfutbol@gmail.com>
2014-03-19 00:28:27 -0400

    Update using current jQuery plugin standards.

--- dist/jquery.textarea_auto_expand.js
+++ dist/jquery.textarea_auto_expand.js
@@ -1,29 +1,54 @@
-(function($){
-  $.fn.textareaAutoExpand = function(){
-    return this.each(function(){
-      var textarea = $(this);
-      var height = textarea.outerHeight();
-      var diff = parseInt(textarea.css('paddingBottom')) + parseInt(textarea.css('paddingTop'));
+/*!
+ * jQuery Textarea Auto Expand plugin
+ * Author: Javier Julio
+ * Licensed under the MIT license
+ */
+;(function ($, window, document, undefined) {
 
-      function containsText(value) {
-        return (value.replace(/\s/g, '').length > 0);
-      }
+  var pluginName = "textareaAutoExpand";
+  var pluginDataName = "plugin_" + pluginName;
+
+  var containsText = function (value) {
+    return (value.replace(/\s/g, '').length > 0);
+  };
+
+  function Plugin(element, options) {
+    this.element = element;
+    this.$element = $(element);
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var height = this.$element.outerHeight();
+      var diff = parseInt(this.$element.css('paddingBottom')) +
+                  parseInt(this.$element.css('paddingTop'));
 
       // Firefox: scrollHeight isn't full height on border-box
-      if (this.scrollHeight + diff <= height) {
+      if (this.element.scrollHeight + diff <= height) {
         diff = 0;
       }
 
-      if (containsText(this.value)) {
-        textarea.height(this.scrollHeight);
+      if (containsText(this.element.value)) {
+        this.$element.height(this.element.scrollHeight);
       }
 
       // keyup is required for IE to properly reset height when deleting text
-      textarea.on('input keyup', function(event) {
+      this.$element.on('input keyup', function(event) {
         $(this)
           .height('auto')
           .height(this.scrollHeight - diff);
       });
-    });
+    }
   };
-})(jQuery);
+
+  $.fn[pluginName] = function (options) {
+    this.each(function() {
+      if (!$.data(this, pluginDataName)) {
+        $.data(this, pluginDataName, new Plugin(this, options));
+      }
+    });
+    return this;
+  };
+
+})(jQuery, window, document);
