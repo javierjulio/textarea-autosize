b21764b79fb68754f28db183ac0393029d4a81c8
Javier Julio <jjfutbol@gmail.com>
2014-04-25 20:48:51 -0400

    Add sample DOM tests to verify phantomjs

--- test/test.js
+++ test/test.js
@@ -1,10 +1,39 @@
-var assert = require('assert')
-
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      array = [1, 2, 3];
+      expect(array.indexOf(5)).to.equal(-1);
+      expect(array.indexOf(0)).to.equal(-1);
     });
+  });
+});
+
+describe("DOM Tests", function () {
+  var el, myEl;
+  
+  before(function(){
+    el = document.createElement("div");
+    el.id = "myDiv";
+    el.innerHTML = "Hi there!";
+    el.style.background = "#ccc";
+    document.body.appendChild(el);
+    
+    myEl = document.getElementById('myDiv');
+  });
+
+  it("is in the DOM", function () {
+    expect(myEl).to.not.equal(null);
+  });
+
+  it("is a child of the body", function () {
+    expect(myEl.parentElement).to.equal(document.body);
+  });
+
+  it("has the right text", function () {
+    expect(myEl.innerHTML).to.equal("Hi there!");
+  });
+
+  it("has the right background", function () {
+    expect(myEl.style.background).to.equal("rgb(204, 204, 204)");
   });
 });
