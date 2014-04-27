6f1c0c1f02eeb547ddc69b5383bc78482e4d6a2a
Javier Julio <jjfutbol@gmail.com>
2014-04-27 14:43:41 -0400

    Remove unrelated tests now that suite is working

--- test/test.js
+++ test/test.js
@@ -1,13 +1,3 @@
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      array = [1, 2, 3];
-      expect(array.indexOf(5)).to.equal(-1);
-      expect(array.indexOf(0)).to.equal(-1);
-    });
-  });
-});
-
 // font size * line height + top/bottom padding + top/bottom border
 // (16 * 1.4) + (6 * 2) + 2
 
@@ -22,54 +12,28 @@ function spacingHeight(textarea) {
     parseInt(textarea.css('borderTopWidth'));
 }
 
-describe("DOM Tests", function () {
-  var el, myEl;
+describe("TextArea", function () {
+  var textarea;
   
   before(function(){
-    el = document.createElement("div");
-    el.id = "myDiv";
-    el.innerHTML = "Hi there!";
-    el.style.background = "#ccc";
-    document.body.appendChild(el);
-    
-    myEl = document.getElementById('myDiv');
+    textarea = $("#js-single-line-textarea").textareaAutoSize();
   });
 
   it('has a rendered height of one line with no text', function() {
-    var textarea = $("#js-single-line-textarea").textareaAutoSize();
-    
     expect(textarea.val()).to.equal('');
     expect(textarea.outerHeight()).to.equal(36);
   });
 
   it('has a rendered height of 2 lines when large enough text entered', function() {
-    var textarea = $("#js-single-line-textarea").textareaAutoSize();
     textarea.val('this is a test with a really long entry').trigger('input');
     
     expect(textarea.outerHeight()).to.equal(58);
   });
 
   it('resizes to one line when deleting all text', function() {
-    var textarea = $("#js-single-line-textarea").textareaAutoSize();
     textarea.val('').trigger('input');
     
     expect(textarea.outerHeight()).to.equal(36);
     expect(textarea.val()).to.equal('');
-  });
-
-  it("is in the DOM", function () {
-    expect(myEl).to.not.equal(null);
-  });
-
-  it("is a child of the body", function () {
-    expect(myEl.parentElement).to.equal(document.body);
-  });
-
-  it("has the right text", function () {
-    expect(myEl.innerHTML).to.equal("Hi there!");
-  });
-
-  it("has the right background", function () {
-    expect(myEl.style.background).to.equal("rgb(204, 204, 204)");
   });
 });
